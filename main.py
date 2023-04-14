fcfe7c9911419984f1aa9622d90171441fe06fcb
Max <ballbobba@gmail.com>
2023-04-14 14:23:44 +0300

    da

---
+++ main.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template, redirect, request, Blueprint, jsonify, make_response, abort
+import datetime
+import os
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField, StringField, IntegerField
+# from wtforms import SelectMultipleField, DateTimeField, SelectField
+from wtforms.validators import DataRequired
+from data import db_session, users
+# from data.db_session import SqlAlchemyBase
+from data.users import User
+# from data.jobs import Jobs
+# from data.news import News
+# from data.hazard_levels import HazardLevel
+# from data.user_resource import UsersResource, UsersListResource
+# import sqlalchemy
+from flask_restful import Api
+
+# from data.jobs_resource import JobsResource, JobsListResource
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+blueprint = Blueprint(
+    'news_api',
+    __name__,
+    template_folder='templates'
+)
+
+db_session.global_init("db/blogs.db")
+dbs = db_session.create_session()
+
+dbs.commit()
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    verificatepassword = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age')
+    position = StringField('Position')
+    speciality = StringField('Speciality')
+    address = StringField("Address")
+    submit = SubmitField('Register')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('base.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.verificatepassword.data:
+            return render_template('register.html', title='Register form',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Register form',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            position=form.position.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='регистрация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
